4f8ae4f6b0f3f42259adb066d9877bc4b9946e62
loveAlakazam <dmsrkd1216@gmail.com>
2022-08-27 15:21:00 +0900

    ADD - Add Router

--- app.js
+++ app.js
@@ -1,49 +1,25 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
 const bodyParser = require("body-parser");
+const http = require("http");
 
-/*
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const dotenv = require("dotenv");
+dotenv.config({ path: path.join(__dirname, "./configs/.env") });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var app = express();
+// Routers
+const userRouter = require("./routes/users");
+const projectRouter = require("./routes/projects");
+const projectReqRouter = require("./routes/projectRequests");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use("/user", userRouter);
+app.use("/prj", projectRouter);
+app.use("/prj/req", projectReqRouter);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const PORT = process.env.PORT;
+http.createServer(app).listen(PORT, () => {
+  console.log(`The Server started port ${PORT} ...`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-*/
-
-module.exports = app;
